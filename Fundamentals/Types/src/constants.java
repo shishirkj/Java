739095b1a07caa71299a0d153aeb30913f4b52a0
Pradyumna Hegde <117185220+Pradyumnahegde@users.noreply.github.com>
2023-02-01 00:04:49 +0530

    Delete Fundamentals directory

--- Fundamentals/Types/src/constants.java
+++
@@ -1,8 +0,0 @@
-public class constants {
-    public static void main(String[] args) {
-     final float pi = 3.142f;
-        System.out.println(pi);
-//        pi = 10;
-//        System.out.println(pi);        error its a constant value
-    }
-}
