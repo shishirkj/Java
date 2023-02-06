95a7cadab87341a8a48c59f6ef3cd9e78c142b6a
Pradyumna Hegde <117185220+Pradyumnahegde@users.noreply.github.com>
2023-02-06 20:21:46 +0530

    Update Car.java

--- OOP's/Classes/src/Car.java
+++ OOP's/Classes/src/Car.java
@@ -8,6 +8,8 @@ public class Car {
         //  or use   public void carName(String name){
            this.name = name;                               // we use this.  when name of parameter is same as field.
     }
+    /*      The keyword "this" in Java is used to refer to the current instance of an object. It is used to access instance variables and methods, and also to refer to the object itself when passing it as an argument to a method. Using "this" helps disambiguate between instance variables and local variables with the same name, and allows the programmer to clearly specify which variables and methods belong to the current object.
+    */
 
     public void carPrice() {
         this.price = price;
